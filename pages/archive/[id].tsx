c4fb05fd51b35ba6ba4c58f15e14a2454e1149a4
JunyupHong <junyup.hong@gmail.com>
2021-02-21 17:28:44 +0900

    [Style] Archive Page Footer 및 background 적용

--- pages/archive/[id].tsx
+++ pages/archive/[id].tsx
@@ -1,5 +1,9 @@
+import Footer from '../../components/common/Footer';
+
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+
+import styled from '@emotion/styled';
 
 type appjamData_T = {
     name: string;
@@ -9,6 +13,34 @@ type appjamData_T = {
         desc: string;
         imageURL: string;
     }>;
+};
+
+const Styled = {
+    ArchiveWrapper: styled.div`
+        background-image: url('/assets/images/archivepage_background.png');
+        background-size: cover;
+        background-position: 1 50%;
+        background-repeat: no-repeat;
+    `,
+    ContentWrapper: styled.div`
+        padding: 240px 400px 140px 400px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        color: white;
+    `,
+    Title: styled.h2`
+        font-size: 60px;
+        font-weight: 700;
+        margin-bottom: 31px;
+    `,
+    Desc: styled.p`
+        font-size: 20px;
+        font-weight: 300;
+        text-align: center;
+    `,
 };
 const Archive = () => {
     const router = useRouter();
@@ -26,11 +58,19 @@ const Archive = () => {
 
     return appjamData ? (
         <>
-            <h1>
-                {appjamData.sequence}th
-                <br />
-                {appjamData.name}
-            </h1>
+            <Styled.ArchiveWrapper>
+                <Styled.ContentWrapper>
+                    <Styled.Title>Archiving</Styled.Title>
+                    <Styled.Desc>
+                        2008년부터 시작된 SOPT는 매 기수마다 11개 이상의 서비스를 선보입니다.
+                        <br />
+                        APP-JAM을 통해 나온 멋진 결과물을 확인하세요!
+                    </Styled.Desc>
+                </Styled.ContentWrapper>
+                <div style={{ padding: '0 240px 100px 240px' }}>
+                    <Footer />
+                </div>
+            </Styled.ArchiveWrapper>
         </>
     ) : (
         <></>
--- pages/archive/index.tsx
+++ pages/archive/index.tsx
@@ -1,5 +1,12 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
 const ArchiveIndex = () => {
-    return <>잘못된 경로입니다.</>;
+    const router = useRouter();
+    useEffect(() => {
+        router.push('/archive/27');
+    });
+    return <></>;
 };
 
 export default ArchiveIndex;
